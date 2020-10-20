143e8a492bcf9b82efb4ab1f4bb7c29e63c913aa
unknown <juanignaciocor92@gmail.com>
2020-10-20 11:47:38 -0300

    final

--- back/db/index.js
+++ back/db/index.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize(
-  "postgres://postgres:strife2348@localhost:5432/challengeBootcamp",
-  { logging: false }
-);
+const db = new Sequelize("postgres://localhost:5432/challengeBootcamp", {
+  logging: false,
+});
 
 module.exports = db;
